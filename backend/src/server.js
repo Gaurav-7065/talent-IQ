9a01c950eb28a2e760fa9a68f91c8303923e792b
Gaurav Jha <jhagaurav214@gmail.com>
2026-05-23 03:02:36 +0530

    fixed import issue

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,8 +5,8 @@ import { connectDB } from "./lib/db.js";
 import { serve } from "inngest/express"
 import { inngest,functions } from './lib/inngest.js'
 import { clerkMiddleware } from "@clerk/express";
-import { chatRoutes } from "./routes/chatRoutes.js";
-import {sessionRoutes} from "./routes/sessionRoute.js"
+import  chatRoutes from "./routes/chatRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js"
 
 const app = express();
 
