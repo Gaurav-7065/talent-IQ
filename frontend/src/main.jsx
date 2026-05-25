a9bbabd5422493d80f0413760db6f36c4ce559c9
Gaurav Jha <jhagaurav214@gmail.com>
2026-05-25 11:06:53 +0530

    deployment issue fixed

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -14,6 +14,7 @@ if (!PUBLISHABLE_KEY) {
 }
 
 const queryClient = new QueryClient();
+console.log(import.meta.env.VITE_API_URL);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
