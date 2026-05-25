5b54e33cb690887159f426920cc2a929d33ade96
Gaurav Jha <jhagaurav214@gmail.com>
2026-05-25 11:15:45 +0530

    deployment issue fixed

--- backend/src/middleware/protectRoute.js
+++ backend/src/middleware/protectRoute.js
@@ -1,38 +1,31 @@
-import { requireAuth } from "@clerk/express";
 import User from "../models/User.js";
 
-export const protectRoute = [
-  requireAuth(),
-  async (req, res, next) => {
-    try {
-      const { userId } = req.auth;
+export const protectRoute = async (req, res, next) => {
+  try {
+    const userId = req.auth?.userId;
 
-      if (!userId) {
-        return res.status(401).json({
-          message: "Unauthorized - invalid token",
-        });
-      }
-
-      // find user in db by clerk ID
-      let user = await User.findOne({ clerkId: userId });
-
-      // auto create user if not found
-      if (!user) {
-        user = await User.create({
-          clerkId: userId,
-        });
-      }
-
-      // attach user to req
-      req.user = user;
-
-      next();
-    } catch (error) {
-      console.error("Error in protectRoute middleware", error);
-
-      res.status(500).json({
-        message: "Internal Server Error",
+    if (!userId) {
+      return res.status(401).json({
+        message: "Unauthorized",
       });
     }
-  },
-];
+
+    let user = await User.findOne({ clerkId: userId });
+
+    if (!user) {
+      user = await User.create({
+        clerkId: userId,
+      });
+    }
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    console.log(error);
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
+};
